7189e15f26d294a14a551b651702b47a1291bc14
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-08-19 15:21:23 +0900

    Dwitter 리펙토링: configuration 반영

---
+++ Node/12-dwitter-configuration/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import "express-async-errors";
+
+import tweetsRouter from "./routes/tweet.js";
+import authRouter from "./routes/auth.js";
+import { config } from "./config.js";
+
+const app = express();
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+app.use(cors());
+
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Something went wrong" });
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+
+app.listen(config.host.port);
